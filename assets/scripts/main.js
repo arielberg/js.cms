4e8d469bfe3fc6a024761dd15793ee33ccf8ad05
Ariel Berg <arielberg@gmail.com>
2020-06-29 17:34:04 +0300

    add js base element in base.html

--- assets/scripts/main.js
+++
@@ -1,7 +0,0 @@
-let matchGitHubPages = window.location.href.match(/(.*)github.io\/([^\/]*)/);
-
-if ( matchGitHubPages ) {
-    let baseElement = document.createElement('base');
-    baseElement.href = matchGitHubPages[0];
-    document.querySelector('head').appendChild(baseElement);
-}
